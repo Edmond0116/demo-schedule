01a14a0ffc5ba635388d4e8bbc626736e08bfb69
Elmon <elmon@nlplab.cc>
2024-05-17 17:22:24 +0900

    fix: add handleSignOut()

--- app/ui/sign-out.tsx
+++ app/ui/sign-out.tsx
@@ -1,11 +1,13 @@
-import { leave } from '../actions'
-import { cookies } from 'next/headers'
-
+// import { leave } from '../actions'
+import { signOut } from 'next-auth/react'
+const handleSignOut = async () => {
+  await signOut({ callbackUrl: '/' })
+}
 export async function SignOut() {
   return (
     <button
       onClick={() => {
-        leave()
+        handleSignOut()
       }}
       // className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
     >
