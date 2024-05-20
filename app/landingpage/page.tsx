f566c82f4dc507d20eef9005e2ff14507d66f6ba
Elmon <elmon@nlplab.cc>
2024-05-20 13:04:50 +0900

    Update page.tsx
    refact: move Chakra UI into intro.tsx

--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -1,11 +1,8 @@
-'use client'
 // import Image from 'next/image'
 import NavLink from '@/app/ui/NavLink'
 import Burger from '@/app/ui/burger'
-import { Center, ChakraProvider, Divider } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-import { Button } from '@chakra-ui/react'
-import { Image, Stack, Text, Heading, SimpleGrid } from '@chakra-ui/react'
+import { NavBar } from '../ui/NavBar'
+import { Intro } from '../ui/intro'
 // import { SignIn } from '@/app/ui/sign-in'
 // import { SignOut } from '../ui/sign-out'
 // 2. Call `extendTheme` and pass your custom values
@@ -16,7 +13,8 @@ export default function Home() {
     // <main className="flex min-h-screen flex-col items-center justify-between p-6">
     <main className="flex min-h-screen flex-rol">
       <div className="w-1/8 sm:w-1/6 md:w-1/4">
-        <NavLink />
+        {/* <NavLink /> */}
+        <NavBar />
         {/* <SignOut /> */}
       </div>
       <div
@@ -63,54 +61,7 @@ export default function Home() {
             </ul>
           </div>
         </div>
-        <ChakraProvider>
-          <div className="p-4 md-flex ">
-            <h1 className="text-orange-400 sm:text-sm md:text-base lg:text-2xl w-fit h-fit z-0">
-              Who am I?
-            </h1>
-          </div>
-          <SimpleGrid
-            spacing={4}
-            templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
-          >
-            <Card className="items-end w-full md:w-auto">
-              <CardBody>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'left',
-                  }}
-                >
-                  <Image
-                    src="/IMG_1153.png"
-                    alt="Tokyo Tower at night"
-                    className="place-items-center"
-                    width={150}
-                    height={150}
-                    borderRadius="lg"
-                  />
-                </div>
-                <Stack mt="6" spacing="3">
-                  <Heading size="md">Living room Sofa</Heading>
-                  <Text className="text-base/6 sm:text-sm md:text-lg lg:text-base">
-                    This sofa is perfect for modern tropical spaces, baroque
-                    inspired spaces, earthy toned spaces and for people who love
-                    a chic design with a sprinkle of vintage design.
-                  </Text>
-                </Stack>
-              </CardBody>
-              <CardFooter>
-                <Button
-                  className="absolute inset-y-0 right-0"
-                  variant="solid"
-                  colorScheme="blue"
-                >
-                  Read more...
-                </Button>
-              </CardFooter>
-            </Card>
-          </SimpleGrid>
-        </ChakraProvider>
+        <Intro />
       </div>
     </main>
   )
